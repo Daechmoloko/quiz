846f14c5bf16f9e6583c38247346453512beb7fe
Daechmoloko <allundercontroll@yandex.ru>
2023-03-05 16:55:01 +0300

    add second quiz and refactoring code"

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@
 require "bundler/setup"
 require "dry/cli"
 require "time"
+require_relative "question_data"
+require_relative "file_writer"
+require_relative "statistiks"
 
 
 module App 
@@ -10,61 +13,90 @@ module App
       extend Dry::CLI::Registry
 
       class RunQuiz < Dry::CLI::Command
+
+        desc "Print name quiz"
+
+        argument :name_quiz, desc: "Название квиза"
+
+        example [
+          "             # Prints name quiz"
+        ]
   
-        def call(*)
-          Generate::Run.new().call
+        def call(name_quiz: 'first_quiz', **)
+          Generate::Run.new.call(name_quiz)
         end
 
       end
     
-      module Generate  
-        require_relative "question_data"
-        require_relative "file_writer"
-        require_relative "statistiks"
+      module Generate 
 
-        class Run < Dry::CLI::Command
-          
-          def call(*)
+        class Run <Dry::CLI::Command
 
+         
+
+          def get_name
             puts "Введите Ваше имя"
-            name = STDIN.gets.strip
-            current_time = Time.now.strftime("%Y-%m-%d_%H:%M:%S")
-            @writer = FileWriter.new('a', {name: name, current_time: current_time})
-            @question_data = QuestionData.new @writer
-            @statistiks = Statistiks.new @writer
+            STDIN.gets.strip 
+          end
 
-            @question_data.collection.each do |next_question|
+          def get_current_time
+            Time.now.strftime("%Y-%m-%d_%H:%M:%S")
+          end
+          
+          def create_report_file(name, current_time)
+            FileWriter.new('a', {name: name, current_time: current_time})
+          end
+
+          def get_raw_question_data(name_quiz) 
+            QuestionData.new name_quiz
+          end
+
+          def entry_report_statistik (writer)
+            Statistiks.new writer
+          end
+
+          def question_answers(question_data, statistiks)
+            question_data.collection.each do |next_question|
               puts next_question
               next_question.display_answers
               user_answer = next_question.find_answer_by(ask_for_answer_char )
         
-              check?(user_answer, next_question.corrent_answer)
+              check?(user_answer, next_question.corrent_answer, statistiks)
             end
-
-            @statistiks.print_report
           end
-
-          private
 
           def ask_for_answer_char 
             loop do
               puts "Ваш ответ: "
-              user_answer_char = STDIN.gets.strip[0].upcase
-              return user_answer_char if user_answer_char.between?('A', 'D')
+              user_answer_char = STDIN.gets.strip[0]
+              return user_answer_char if !user_answer_char.nil? && user_answer_char.upcase.between?('A', 'D')
 
-              puts "Ответ должен быть от А до D!"
+              puts "Ответ должен быть от А до D, и не пустым!"
             end
           end
 
-          def check?(user_answer, corrent_answer)
+          def check?(user_answer, corrent_answer, statistiks)
             if user_answer == corrent_answer
               puts "Верно!"
-              @statistiks.corrent 
+              statistiks.corrent 
             else
               puts "Неверно!"
               puts "Верный ответ #{corrent_answer}"
-              @statistiks.incorrent 
+              statistiks.incorrent 
             end
+          end
+
+          def call(name_quiz, **)
+            name = get_name
+            current_time = get_current_time
+            writer = create_report_file(name, current_time)
+            writer.write("\n\n*** Имя пользователя #{name} ***\n")
+            writer.write("\n\n*** Названия квиза #{name_quiz} ***\n")
+            writer.write("\n\n*** Время создания #{current_time} ***\n\n")
+            question_data = get_raw_question_data(name_quiz)
+            statistiks = entry_report_statistik(writer)
+            question_answers(question_data, statistiks)
+            statistiks.print_report
           end
         end
       end
--- question_data.rb
+++ question_data.rb
@@ -4,16 +4,15 @@ class QuestionData
 
   attr_reader :collection
 
-  def initialize(filename = 'question.yml', writer)
-    @collection = load_from filename
-    @writer = writer
-    @writer.write("\n\n*** #{@collection.inspect} ***\n\n")
+  def initialize(filename = 'question.yml', name_quiz)
+    @collection = load_from(filename, name_quiz)
   end
 
   private
 
-  def load_from(filename)
-    YAML.safe_load_file(filename, symbolize_names: true).last[:question].shuffle.map do |q_data|
+  def load_from(filename, name_quiz)
+    YAML.safe_load_file(filename, symbolize_names: true).select {|arg| arg[:name] == name_quiz}.
+    last[:question].shuffle.map do |q_data|
       Question.new(q_data[:title], q_data[:answers])
     end
   end
